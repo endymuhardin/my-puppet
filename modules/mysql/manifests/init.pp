cb3b072b452e7b2965ee1cd128642f7825048992
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-03 16:41:27 +0700

    modul installer mysql

---
+++ modules/mysql/manifests/init.pp
@@ -0,0 +1,48 @@
+class mysql::params {
+    $mysql_password = "admin"
+}
+
+class mysql::install {
+    package { [ "mysql-server"]: 
+        ensure => present,
+        require => Class["ubuntubase"]
+    }
+}
+
+class mysql::service {
+    service {"mysql":
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+        hasrestart => true,
+        require => Class["mysql::install"]
+    }
+}
+
+
+class mysql::config {
+    exec { "set-mysql-password":
+        unless => "mysqladmin -uroot -p$mysql::params::mysql_password status",
+        path => ["/bin", "/usr/bin"],
+        command => "mysqladmin -uroot password $mysql::params::mysql_password",
+        require => Class["mysql::service"],
+    }
+}
+
+define mysqldb( $user, $password ) {
+    exec { "create-${name}-db":
+        unless => "/usr/bin/mysql -uroot -p$mysql::params::mysql_password ${name}",
+        command => "/usr/bin/mysql -uroot -p$mysql::params::mysql_password -e \"create database ${name};\"",
+        require => Class["mysql::service"],
+    }
+
+    exec { "grant-${name}-db":
+        unless => "/usr/bin/mysql -u${user} -p${password} ${name}",
+        command => "/usr/bin/mysql -uroot -p$mysql::params::mysql_password -e \"grant all on ${name}.* to ${user}@localhost identified by '$password';\"",
+        require => [Class["mysql::service"], Exec["create-${name}-db"]]
+    }
+}
+
+class mysql {
+    include mysql::params, mysql::install, mysql::service, mysql::config
+}
