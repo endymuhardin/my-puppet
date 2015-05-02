ee3912e007517e82b077845fa59a8358d5aed2ad
Endy Muhardin <endy.muhardin@gmail.com>
2015-05-02 10:36:33 +0700

    update package list ubuntudesktop

--- modules/ubuntudesktop/manifests/init.pp
+++ modules/ubuntudesktop/manifests/init.pp
@@ -8,20 +8,12 @@ class ubuntudesktop {
       ,"unrar"
       ,"gimp"
       ,"gimp-plugin-registry"
-      ,"gtk-recordmydesktop"
-      ,"network-manager-openvpn"
-      ,"network-manager-openvpn-gnome"
-      ,"network-manager-pptp"
-      ,"network-manager-pptp-gnome"
-      ,"network-manager-vpnc"
-      ,"network-manager-vpnc-gnome"
       ,"inkscape"
       ,"ubuntu-restricted-extras"
       ,"build-essential"
       ,"nautilus-dropbox"
-      ,"chromium-browser"
       ,"shutter"
-      ,"skype"
+      ,"kazam"
     ]:
     ensure => present
   }
