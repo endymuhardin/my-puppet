091eccb708ccc387283cd5836fdd7ed31607a6f6
Endy Muhardin <endy.muhardin@gmail.com>
2015-06-29 18:50:17 +0700

    add cinelerra

--- modules/video/manifests/init.pp
+++ modules/video/manifests/init.pp
@@ -1,12 +1,14 @@
 class video::sources {
   apt::ppa{ 'ppa:maarten-baert/simplescreenrecorder': }
   apt::ppa{ 'ppa:openshot.developers/ppa': }
+  apt::ppa{ 'ppa:cinelerra-ppa/ppa': }
 }
 
 class video::install {
   package { [
       "simplescreenrecorder",
-      "openshot"
+      "openshot",
+      "cinelerra-cv"
     ]:
     ensure => present,
     require => Class["video::sources"]
