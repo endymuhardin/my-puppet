a0c3830b83aa2c5450ecf4033797d3438ae46b0f
Endy Muhardin <endy.muhardin@gmail.com>
2015-07-04 23:07:34 +0700

    install hugin

--- modules/photography/manifests/init.pp
+++ modules/photography/manifests/init.pp
@@ -1,10 +1,12 @@
 class photography::sources {
   apt::ppa{ 'ppa:pmjdebruijn/darktable-release': }
+  apt::ppa{ 'ppa:hugin/hugin-builds': }
 }
 
 class photography::install {
   package { [
-      "darktable"
+      "darktable",
+      "hugin"
     ]:
     ensure => present,
     require => Class["photography::sources"]
